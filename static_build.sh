e168d5fc2ae833c61267be04410304eff2fc45c7
Bruno Vieira Costa <brunopsycon@gmail.com>
2016-08-03 17:11:59 -0300

    static_build comments

--- static_build.sh
+++ static_build.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+#Use:  ./static_build.sh [directory] [filename]
+#Example: ./static_build.sh assets final
+#   final.css   final.js
 find "$1" -name "*.css" | xargs cat > "$2.css"
 find "$1" -name "*.js" | xargs cat > "$2.js"
