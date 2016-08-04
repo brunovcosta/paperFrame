d1dd6f0c68a4df86b132e13e8ea4158e10e97ca4
Bruno Vieira Costa <brunopsycon@gmail.com>
2016-08-04 11:11:59 -0300

    FileChangeListener

---
+++ FileChangeListener.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+=begin
+This script checks if there is a file change and call a given trigger
+
+Use mode: ./FileChangeListener.rb [dir to watch] [cmd]
+Example: ./FileChangeListener.rb assets "cordova prepare browser"
+=end
+$md5 = `tar -cf - ./#{ARGV[0]} | md5sum`
+loop do
+	$new_md5 = `tar -cf - ./#{ARGV[0]} | md5sum`
+	if $new_md5!=$md5
+		$md5 = $new_md5
+		puts "change detected"
+		puts "#{ARGV[1]}"
+		`#{ARGV[1]}`
+		puts "done!\n\n"
+	end
+	sleep 1
+end
